f58b6d48f7725f90c0340146c6193001144385eb
Matt Hernandez <maherna@microsoft.com>
2018-03-14 11:02:56 -0700

    Add song serch functionality.
    Everything is in there now...

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="search-intro">
+      <form @submit="search">
+          <label for="search-term">Search for a Song</label>
+          <fieldset>
+            <input id="search-term" name="search-term" value="">
+            <input type="submit" value="Search">
+          </fieldset>
+      </form>
+    </div>
+    <ul>
+      <MediaItem
+        v-for="item in results"
+        v-bind:media="item"
+      ></MediaItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import MediaItem from './components/MediaItem.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: { MediaItem },
+  data: function () {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    search: function (e) {
+      e.preventDefault()
+
+      axios.get('https://itunes.apple.com/search?media=music&entity=song&term=' + document.getElementsByName('search-term')[0].value)
+        .then((response) => {
+          console.log(response)
+          this.results = response.data.results
+        }).catch(console.error.bind(console, 'Fetch error:'))
+    }
   }
 }
 </script>
@@ -21,8 +49,79 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #333;
+}
+
+ul {
+  margin: 210px 0 0 0;
+  padding: 0;
+}
+
+fieldset {
+  border: none;
+  text-align: center;
+}
+
+label, input {
+  display: block;
+  margin: 5px auto 0;
+}
+
+label {
+  text-align: center;
+}
+
+input {
+  display: inline-block;
+  border: none;
+  padding: 10px 10px;
+  width: 400px;
+  font-size: 16px;
+}
+
+input[type=submit] {
+  width: 140px;
+  background-color: #333;
+  color: #DDD;
+  padding: 10px 10px;
+  text-align: center;
+  text-decoration: none;
+}
+
+input[type=submit]:hover {
+  cursor: pointer;
+}
+
+img {
+  display: inline-block;
+}
+
+form {
+  margin-top: 30px;
+}
+
+label[for=search-term]{
+  color: #DDD;
+  font-size: 2.5rem;
+  font-family: fantasy;
+}
+
+.search-intro {
+  position: fixed;
+  width: 100%;
+  height: 200px;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background: url(./assets/musics.jpg) bottom center no-repeat;
 }
 </style>
---
+++ src/components/MediaItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <li class="media-item" v-bind:key="media.trackId">
+    <img v-bind:src="media.artworkUrl100">
+    <div class="details">
+      <h1 class="track">{{ media.trackName }}</h1>
+      <span>by</span>
+      <h2 class="artist">{{ media.artistName }}</h2>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name:'MediaItem',
+  props: ['media']
+}
+</script>
+
+<style scoped>
+.track {
+  max-height: 55px;
+  overflow: hidden;
+}
+
+.track, .artist {
+  margin: 0;
+}
+
+h1 {
+  font-size: 1rem;
+}
+
+h2 {
+  font-size: .9rem;
+}
+
+span {
+  margin: 7px auto;
+}
+
+li.media-item {
+  display: inline-block;
+  width: 290px;
+  height: 105px;
+  margin: 10px 5px;
+  background-color: #EEE;
+  padding: 15px;
+  overflow: hidden;
+  text-align: left;
+}
+
+li.media-item span {
+  display: block;
+  overflow: hidden;
+}
+
+div.details {
+  display: inline-block;
+  position: absolute;
+  width: 180px;
+  margin-left: 5px;
+}
+</style>
